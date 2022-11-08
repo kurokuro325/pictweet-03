d7a7f66bc11dc5b24851c4cbf9e0aae5d244006e
kurokuro325 <katapa.zari.123@gmail.com>
2022-11-08 14:26:10 +0900

    ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   resources :tweets
-  resources :users, only: :show
+  resources :users, only: [:show] do
+  member do
+    get :follows, :followers
+  end
+end
+  resource :relationships, only: [:create, :destroy]
 end
