10e4a691a4195a4bf21120b2153b6e5eb727e16c
kurokuro325 <katapa.zari.123@gmail.com>
2022-11-13 18:34:15 +0900

    tweetテーブルにstatusカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_045059) do
+ActiveRecord::Schema.define(version: 2022_11_13_093033) do
 
   create_table "relationships", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "follower_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_11_08_045059) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "status", default: 0, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
